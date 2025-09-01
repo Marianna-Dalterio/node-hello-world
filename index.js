1e521f810d7f2dd68a5863c4f6b9ea9b1739aa14
Marianna-Dalterio <mariannadalterio00@gmail.com>
2025-09-01 19:03:10 +0200

    bonus

--- index.js
+++ index.js
@@ -5,3 +5,9 @@ console.log("Hello World");
 //3 lancio lo script con npm start
 
 //4 lancio il live reload con npm run watch, per tornare a scrivere nel terminale CTRL+C
+
+//5 bonus
+
+const hello = process.argv[2] || "Boolean";
+console.log(process.argv);
+console.log(`Hello ${hello}`);
