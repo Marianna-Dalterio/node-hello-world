6878e788796c7849b5e2bf2976819bb9747c2a6a
Marianna-Dalterio <mariannadalterio00@gmail.com>
2025-09-01 18:49:14 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,7 @@
+console.log("Hello World");
+
+//2 eseguo il file dal terminale con node index.js
+
+//3 lancio lo script con npm start
+
+//4 lancio il live reload con npm run watch, per tornare a scrivere nel terminale CTRL+C
